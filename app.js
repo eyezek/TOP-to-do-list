befcfdd221894eaec47b036d71eec479196e75cb
eyezek <71928231+eyezek@users.noreply.github.com>
2022-06-26 16:04:02 -0400

    still figuring everything out haha

--- app.js
+++ app.js
@@ -19,6 +19,25 @@ newList.addEventListener("click", function () {
   modal.style.display = "block";
 });
 
-submit.addEventListener("click", function () {
-  toDoFactory(title.value, description.value, dueDate.value, priority.value);
+function displayToDos() {
+  for (let i = 0; i < lists.length; i++) {
+    const newToDoDisplay = document.createElement("div");
+    newToDoDisplay.classList.add(".lists");
+    document.querySelector(".lists").appendChild(newToDoDisplay);
+    newTitleDisplay = document.createElement("h2");
+    newTitleDisplay.innerHTML = document.getElementById("title").value;
+    newToDoDisplay.appendChild(newTitleDisplay);
+  }
+}
+
+submit.addEventListener("click", function getToDo(event) {
+  event.preventDefault();
+  const newToDo = toDoFactory(
+    (this.title = document.getElementById("title").value),
+    (this.description = document.getElementById("description").value),
+    (this.dueDate = document.getElementById("duedate").value),
+    (this.priority = document.getElementById("priority").value)
+  );
+  lists.push(newToDo);
+  displayToDos(lists);
 });
